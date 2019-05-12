ea2bec88dff9deae29fe767fffd6d093e9fdc463
Marshall Offutt <marshalloffutt@gmail.com>
2019-05-12 12:11:20 -0500

    larry

---
+++ favorite_languages.py
@@ -0,0 +1,8 @@
+favorite_languages = {
+  'larry': 'python',
+  'gary': 'c#',
+  'barry': 'ruby',
+  'jerry': 'python',
+}
+
+print("Gary's favorite language is " + favorite_languages['gary'].title() + ".")
---
+++ person.py
@@ -0,0 +1,8 @@
+larry = {
+  'first_name': 'larry',
+  'last_name': 'whizbang',
+  'age': '54',
+  'city': 'nashville',
+}
+
+print(larry['first_name'].title() + " " + larry['last_name'].title() + " is " + larry['age'] + " years old, and lives in " + larry['city'].title() + ".")
